47d1e9beab16003f5cb32a58fee4514d6d29f940
switzerit <contact.switzerit@gmail.com>
2026-06-10 10:47:40 +0000

    fix: désactiver SW complètement

--- public/sw.js
+++ public/sw.js
@@ -1,25 +1,7 @@
-const CACHE = 'varman-v2'
-const ASSETS = ['/', '/index.html']
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)))
-  self.skipWaiting()
-})
-
+self.addEventListener('install', () => self.skipWaiting())
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))))
-  self.clients.claim()
-})
-
-self.addEventListener('fetch', e => {
-  const url = new URL(e.request.url)
-  // Ignorer les requêtes API et non-GET
-  if(e.request.method !== 'GET') return
-  if(url.hostname === 'api.varman.ch') return
-  if(url.pathname.includes('favicon')) return
-  if(url.pathname.includes('icon')) return
-  // Pour les autres, réseau d'abord puis cache
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+    .then(() => self.clients.claim())
   )
 })
